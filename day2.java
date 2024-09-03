f5f00db64ab2016f152d7f0d28ff7ea5e6f7aa0b
MuhammadAliSadikin <mhmmdali1306@gmail.com>
2024-09-03 17:12:35 +0800

    Hari 2
    Method Main

---
+++ day2.java
@@ -0,0 +1,9 @@
+public class day2 {
+    public static String siswa(String nama){
+        return nama;
+    }
+    public static void main(String[]args){
+        System.out.println(siswa("Muhammad Ali Sadikin"));
+        
+    }
+}
