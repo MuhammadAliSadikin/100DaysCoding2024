0b3b39025c72ee2bed4893f6b45ad756d96b32d0
MuhammadAliSadikin <mhmmdali1306@gmail.com>
2024-09-04 12:16:44 +0800

    Hari 3
    System.out.println();

---
+++ day3.java
@@ -0,0 +1,14 @@
+package pkg100h_ngoding;
+
+public class day3 {
+
+    public static void main(String[] args) {
+        String nm, almt;
+        nm = "Muhammad Ali Sadikin";
+        almt = "Tapango";
+        int umr = 20;
+        System.out.println("Nama \t: " + nm + "\nAlamat \t: " + almt);
+        System.out.println("Umur \t: " + umr + " th");
+
+    }
+}
