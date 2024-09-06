f58a8d2446c020fecafb74a34326fcfd30ce77cb
MuhammadAliSadikin <mhmmdali1306@gmail.com>
2024-09-06 17:18:08 +0800

    hari 5
    Penggunaan komentar

--- day5.java
+++ day5.java
@@ -6,23 +6,25 @@ public class day5 {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        //println : mencetak teks ke konsol dan menambahkan newline(\n)di akhir.
+         /*Permintaan input dari pengguna*/
         System.out.println("Masukan Biodata Anda");
         System.out.println("---------------------------------------------");
-        //print : mencetak teks ke konsol tanpa pinda baris.
+         /*println : mencetak teks ke konsol dan menambahkan newline(\n)di akhir.*/
         System.out.print("Masukan Nama Anda \t: ");
         String nama = input.nextLine();
         System.out.print("Umur \t\t\t: ");
         int umur = input.nextInt();
         System.out.print("Tiggi Badan \t\t: ");
+         //print : mencetak teks ke konsol tanpa pinda baris.
         double tinggi = input.nextDouble();
         System.out.println("---------------------------------------------");
-        //printf :mencetak teks ke konsol dengan format yang ditentukan.
-        //Seperti penggunaan %s untuk String,%d untuk integer,%.2f untuk Double.
         System.out.printf("Nama :%s \n", nama);
         System.out.printf("Umur :%d tahun \n", umur);
         System.out.printf("Tinggi : %.2f cm\n", tinggi);
-        input.close();
+         /*printf :mencetak teks ke konsol dengan format yang ditentukan.
+         *Seperti penggunaan %s untuk String,%d untuk integer,%.2f untuk Double.
+         *format output dengan placeholder.*/
+        input.close();//Menutup objek dari Scanner
 
     }
 
