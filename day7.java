1fb88131cc1ad80eb70c9dfea0b2b405a48ed211
MuhammadAliSadikin <mhmmdali1306@gmail.com>
2024-09-08 12:32:42 +0800

    Hari 7
    Pemberian nilai pada  variabel yang telah di deklarasi

---
+++ day7.java
@@ -0,0 +1,21 @@
+package pkg100h_ngoding;
+
+public class day7 {
+    public static void main(String[] args) {
+       //Pemberian Nilai Dalam Variabel
+       String a ="Muhammad Ali SAdiikin";
+       int b = 20;
+       double c = 1.65;
+       float d = 45.f;
+       char e = 'L';
+       boolean f = true;
+        System.out.println("Nama \t\t: "+a);
+        System.out.println("Umur \t\t: "+b+"th");
+        System.out.println("Tinggi \t\t: "+c+" m");
+        System.out.println("Berat \t\t: "+d+" kg");
+        System.out.println("Gender (P/L)\t: "+e);
+        
+  
+    
+}
+}
