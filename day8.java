49fffcf7590264b3e74c777351c1b6f1dc52255f
MuhammadAliSadikin <mhmmdali1306@gmail.com>
2024-09-09 15:04:24 +0800

    Hari 8
    Update Variabel

---
+++ day8.java
@@ -0,0 +1,32 @@
+package pkg100h_ngoding;
+
+public class day8 {
+    public static void main(String[] args) {
+        //Pemberian Nilai Dalam Variabel
+        String a = "Muhammad Ali SAdiikin";
+        int b = 20;
+        double c = 1.65;
+        float d = 45.f;
+        char e = 'L';
+        boolean f = true;
+        System.out.println("Nama \t\t: " + a);
+        System.out.println("Umur \t\t: " + b + "th");
+        System.out.println("Tinggi \t\t: " + c + " m");
+        System.out.println("Berat \t\t: " + d + " kg");
+        System.out.println("Gender (P/L)\t: " + e);
+        System.out.println("---------------------------------------");
+        //Update Variable
+        a="ikan cupang";
+        b=10;
+        c=2.5;
+        d=5.f;
+        e='J';
+        f=true;
+        System.out.println("Jenis Ikan \t: " + a);
+        System.out.println("Harga \t\t: " + b +"K");
+        System.out.println("Panjang \t: " + c + " m");
+        System.out.println("Berat \t\t: " + d + " g");
+        System.out.println("Hidup \t\t: " + e);
+    }
+ 
+}
