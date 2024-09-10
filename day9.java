76589f500a7065bf6f72c379d9e92ef03eb0d1c7
MuhammadAliSadikin <mhmmdali1306@gmail.com>
2024-09-10 11:42:11 +0800

    Hari 9
    Variabel Final

--- day9.java
+++ day9.java
@@ -4,7 +4,9 @@ public class day9 {
     public static void main(String[] args) {
         //Variabel final
         final int max_value =100;
-        max_value =200;
+        /* max_value =200; akan menyebabkan eror 
+           karena nilai max_value tidak dapat diubah
+        */
         System.out.println("Nilai Maksimum : "+max_value);
     }
 }
