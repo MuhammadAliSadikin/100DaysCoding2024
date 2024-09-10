45438f251c6906c264b6ae6f4d61f0d4ea86b2a4
MuhammadAliSadikin <mhmmdali1306@gmail.com>
2024-09-10 11:35:03 +0800

    Hari 9
    Final Variabel

---
+++ day9.java
@@ -0,0 +1,10 @@
+package pkg100h_ngoding;
+
+public class day9 {
+    public static void main(String[] args) {
+        //Variabel final
+        final int max_value =100;
+        max_value =200;
+        System.out.println("Nilai Maksimum : "+max_value);
+    }
+}
