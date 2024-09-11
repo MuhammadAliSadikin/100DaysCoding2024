48903d04b053d5841eec1a83561f8df35104f671
MuhammadAliSadikin <mhmmdali1306@gmail.com>
2024-09-11 16:36:46 +0800

    Hari 10
    Tipe Data : Byte dengan rentang nilai -128 sampai 127

---
+++ day10.java
@@ -0,0 +1,27 @@
+package pkg100h_ngoding;
+
+public class day10 {
+    public static void main(String[] args) {
+        //Tipe Data:Byte rentang nilai -128 hingga 127
+        byte a = -128;
+        byte b = 1;
+        byte c = 2;
+        byte d = 3;
+        byte e = 4;
+        byte f = 5;
+        byte g = 6;
+        byte h = 7;
+        byte i = 8;
+        byte j = 127;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+        System.out.println(i);
+        System.out.println(j);
+    }
+}
