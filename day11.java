69981163804e2cabc195f579062df1093f24e2e1
MuhammadAliSadikin <mhmmdali1306@gmail.com>
2024-09-12 10:03:23 +0800

    Hari 11
    Tipe Data : short dengan rentang nilai -32768 sampai dengan 32767

---
+++ day11.java
@@ -0,0 +1,27 @@
+package pkg100h_ngoding;
+
+public class day11 {
+    public static void main(String[] args) {
+        // Tipe Data : short dengan rentang nilai -32768 sampai 32767
+        short a = -32768;
+        short b = 1;
+        short c = 10;
+        short d = 20;
+        short e = 30;
+        short f = 40;
+        short g = 50;
+        short h = 60;
+        short i = 70;
+        short j = 32767;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+        System.out.println(i);
+        System.out.println(j);
+    }
+}
