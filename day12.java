bd36a0a0c44cb4f93f637e19af9fabee987f8727
MuhammadAliSadikin <mhmmdali1306@gmail.com>
2024-09-13 14:19:01 +0800

    Hari 12
    Deklarasi dan Inisialisasi Variabel dengan tipe data integer

---
+++ day12.java
@@ -0,0 +1,28 @@
+package pkg100h_ngoding;
+
+public class day12 {
+
+    public static void main(String[] args) {
+        /* Variabel tipe data:integer
+            Bilangan Ganjil*/
+        int jambu = 7; int apel = 5;
+        int rambutan = 9; int mangga = 1; 
+        int nanas = 3; 
+        int delima = 27;
+        // Bilangan Genap
+        int alpukat,pisang,jeruk,markisa;
+        alpukat = 8;
+        pisang = 10;
+        jeruk = 4;
+        markisa = 100;
+        // Bilangan Prima
+        int sirsak = 13; 
+        int buah_naga = 23; 
+        int langsat = 5; 
+        int anggur = 11;
+
+        System.out.printf("Bilangan Ganjil = %d,%d,%d,%d,%d,%d\n", jambu, apel, rambutan, mangga, nanas, delima);
+        System.out.println("Bilangan Genap  = " + alpukat + "," + pisang + "," + jeruk + "," + markisa);
+        System.out.printf("Bilangan Prima  = %d,%d,%d,%d\n",sirsak,buah_naga,langsat,anggur);
+    }
+}
