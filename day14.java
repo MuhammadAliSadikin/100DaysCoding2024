7f2082b742705589c91cd87f37dbe3434c6c4716
MuhammadAliSadikin <mhmmdali1306@gmail.com>
2024-09-15 15:46:01 +0800

    Hari 14
    Tipe data long

---
+++ day14.java
@@ -0,0 +1,28 @@
+package pkg100h_ngoding;
+
+public class day14 {
+
+    public static void main(String[] args) {
+        //Deklarasi Variabel dengan tipe data:long
+        long a, b, c, d, e, f, g, h, i, j;
+        a = 10;
+        b = 20;
+        c = 30;
+        d = 40;
+        e = 50;
+        f = 60;
+        g = 70;
+        h = 80;
+        i = 90;
+        j = 100;
+        //Kemudian di cetak menggunakan printf
+        System.out.printf("Angka 1 = %d\nAngka 2 = %d\nAngka 3 = %d\nAngka 4 = %d\nAngka 5 = %d\n", a, b, c, d, e);
+        System.out.printf("Angka 6 = %d\nAngka 7 = %d\nAngka 8 = %d\nAngka 9 = %d\nAngka 10 = %d\n", f, g, h, i, j);
+        //Inisialisasi Variabel Dengan Tipe data :long
+        long k = 110;
+        long l = 120;
+        long m = 130;
+        long n = 140;
+        System.out.println("Angka 11 = " + k + "\nAngka 12 = " + l + "\nAngka 13 = " + m + "\nAngka 14 = " + n);
+    }
+}
