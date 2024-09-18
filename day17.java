e4a74703890a76f534c0abde661cb48881f02f71
MuhammadAliSadikin <mhmmdali1306@gmail.com>
2024-09-18 16:05:22 +0800

    Hari 17
    Tipe Data char

---
+++ day17.java
@@ -0,0 +1,32 @@
+package pkg100h_ngoding;
+
+public class day17 {
+    public static void main(String[] args) {
+        //Tipe Data Char
+        char a,b,c,d,e,f,g,h,i,j,k;
+        a = 'I';
+        b = 'N';
+        c = 'F';
+        d = 'O';
+        e = 'R';
+        f = 'M';
+        g = 'A';
+        h = 'T';
+        i = 'I';
+        j = 'K';
+        k = 'A';
+        //Dicetak Vertikal Menggunakan printf
+        System.out.printf("%c\t%c\t%c\t%c\t%c\t",a,b,c,d,e);
+        //Dicetak Vertikal menggunakan print
+        System.out.print(f+"\t"+g+"\t"+h+"\t"+i+"\t"+j+"\t"+k+"\n");
+        //Update Variabel a,b,c,d
+        a = '2';
+        b = '0';
+        c = '2';
+        d = '4';
+        //Dicetak Menggunakan Println
+        System.out.println("");
+        System.out.println(a+"\t"+b+"\t"+c+"\t"+d);
+    }
+    
+}
