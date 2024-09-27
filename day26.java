b93c6027113c0d78d2debae67d62d65b02fedc7e
MuhammadAliSadikin <mhmmdali1306@gmail.com>
2024-09-27 21:02:32 +0800

    Hari 26
    Character

---
+++ day26.java
@@ -0,0 +1,49 @@
+package day26;
+
+public class day26 {
+    public static void main(String[]args){
+        //Tipe data reference Character
+        Character a , b , c , d , e , f , g;
+        a = 'M';
+        b = 'E';
+        c = 'N';
+        d = 'Y';
+        e = 'A';
+        f = 'L';
+        g = 'A';
+        //Dicetak Menggunakan printl
+        System.out.print(a);
+        System.out.print(b);
+        System.out.print(c);
+        System.out.print(d);
+        System.out.print(e);
+        System.out.print(f);
+        System.out.print(g);
+        //Update
+        a ='A';
+        b ='B';
+        c ='A';
+        d ='N';
+        e ='G';
+        f ='K';
+        g ='U';
+        //Dicetak Menggunakan printf
+        System.out.printf("\t%c",a);
+        System.out.printf("%c",b);
+        System.out.printf("%c",c);
+        System.out.printf("%c",d);
+        System.out.printf("%c",e);
+        System.out.printf("%c",f);
+        System.out.printf("%c\n",g);
+        //Inisialisasi variabel baru
+        Character h ='O';
+        Character i ='K';
+        System.out.println(h);
+        System.out.println(i);
+        
+        
+        
+        
+        
+    }
+}
