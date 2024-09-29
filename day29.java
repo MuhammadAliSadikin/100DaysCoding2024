c9b89706bfc057f16f11f99b1652710a5bb90992
MuhammadAliSadikin <mhmmdali1306@gmail.com>
2024-09-29 22:41:39 +0800

    Hari 29
    Reference Boolean

---
+++ day29.java
@@ -0,0 +1,36 @@
+package pkg100h_ngoding;
+
+public class day29 {
+
+    public static void main(String[] args) {
+        //Tipe data reference Boolean
+        System.out.println("----------------------------");
+        System.out.println("Tipe data Reference Boolean");
+        System.out.println("----------------------------");
+        Boolean True = true;
+        Boolean False = false;
+        //Dicetak menggunakan println
+        System.out.println("Boolean : " + True);
+        System.out.println("Boolean : " + False);
+        Boolean a = 1 < 6;
+        Boolean b = 10 == (5 + 5);
+        Boolean c = 5 > 2;
+        Boolean d = (5 * 5) == 25;
+        //Dicetak menggunakan printf
+        System.out.printf("1 < 6 :%b\n",a );
+        System.out.printf("10 sama dengan 5 + 5 : %b\n",b);
+        System.out.printf("5 >  2 : %b\n",c);
+        System.out.printf("5 x 5 = 25 : %b\n",d);
+        Boolean hujan = true;
+        Boolean cerah = false;
+        if (hujan && cerah) {
+            System.out.print("Hari ini turun hujan\n"+hujan);
+        }else if(cerah || hujan){
+            System.out.print("Hari ini cerah\n"+cerah);
+        }else{
+            System.out.println("Hari ini Mendung");
+        }
+           System.out.println("");       
+     
+    }
+}
