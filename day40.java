a0ef72f2c47641125397b6e3fdaf925dd195b3f5
MuhammadAliSadikin <mhmmdali1306@gmail.com>
2024-10-11 17:15:20 +0800

    Hari 40
    operator pebandingan

---
+++ day40.java
@@ -0,0 +1,33 @@
+package pkg100h_ngoding;
+
+import java.util.Scanner;
+
+public class day40 {
+
+    public static void main(String[] args) {
+        //Operator perbandingan
+        Scanner ip = new Scanner(System.in);
+        System.out.print("Nilai A : ");
+        int a = ip.nextInt();
+        System.out.print("Nilai B : ");
+        int b = ip.nextInt();
+        if (a > b) {
+            System.out.println(a + " > " + b);
+        } else if (a < b) {
+            System.out.println(a + " < " + b);
+        } else {
+            System.out.println(a + " = " + b);
+        }
+        System.out.print("Nilai C : ");
+        float c = ip.nextInt();
+        System.out.print("Nilai D : ");
+        float d = ip.nextInt();
+        if (c > d) {
+            System.out.println(c + " > " + d);
+        } else if (c < d) {
+            System.out.println(c + " < " + d);
+        } else {
+            System.out.println(c + " = " + d);
+        }
+    }
+}
