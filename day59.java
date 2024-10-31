9e0320f39d8dfc62ae44487346cb5c8156231959
MuhammadAliSadikin <mhmmdali1306@gmail.com>
2024-10-31 19:35:05 +0800

    Hari 59

---
+++ day59.java
@@ -0,0 +1,13 @@
+package pkg100h_ngoding;
+
+import java.util.Arrays;
+
+public class day59 {
+
+    public static void main(String[] args) {
+        int[] Angka = {1, 2, 3, 4, 5, 6, 7};
+        for (int i = 0; i < Angka.length; i++) {
+            System.out.println("Index ke-"+i+" Adalah : "+Angka[i]); 
+        }
+    }
+}
