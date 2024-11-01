5c87b6ecaa850d8e31f3e03e5c4ef2be25312e02
MuhammadAliSadikin <mhmmdali1306@gmail.com>
2024-11-01 16:12:09 +0800

    Hari 61

---
+++ day61.java
@@ -0,0 +1,37 @@
+package pkg100h_ngoding;
+
+import java.util.Scanner;
+
+public class day61 {
+
+    public static void main(String[] args) {
+        Scanner ip = new Scanner(System.in);
+        for (;;) {
+            System.out.print("Masukan Angka : ");
+            int a = ip.nextInt();
+            if (a % 2 == 0) {
+                System.out.println("Angka Genap Tercetak");
+                break;
+            }
+
+        }
+        for (;;) {
+            System.out.print("Masukan Angka : ");
+            int b = ip.nextInt();
+            if (b % 2 == 1) {
+                System.out.println("Angka Ganjil Tercetak");
+                break;
+            }
+        }
+        int stop = 1;
+        while (true) {   
+            System.out.println(stop+"Halo");
+            stop++;
+            if (stop >= 5) {
+                break;
+            }
+
+        }
+
+    }
+}
