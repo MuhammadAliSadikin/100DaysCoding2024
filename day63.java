58b7a1187c450f2eeafe10714069e6a857ec7d1b
MuhammadAliSadikin <mhmmdali1306@gmail.com>
2024-11-03 19:19:20 +0800

    Hari 63

---
+++ day63.java
@@ -0,0 +1,30 @@
+package pkg100h_ngoding;
+
+import java.util.Scanner;
+
+public class day63 {
+
+    public static void main(String[] args) {
+        System.out.println("Angka Genap Dilewati");
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0) {
+
+                continue;
+            }
+
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        System.out.println("Angka Ganjil dilewati");
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 1) {
+                continue;
+
+            }
+
+            System.out.print(i + " ");
+
+        }System.out.println();
+    }
+
+}
